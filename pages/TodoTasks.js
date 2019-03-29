f35e328ad364a3bb33efbb062d57a1c003a18f76
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-29 10:05:03 +0100

    Retrieved todos from props and described how they should be displayed

--- pages/TodoTasks.js
+++ pages/TodoTasks.js
@@ -1,5 +1,15 @@
 class TodoTasks extends React.Component
 {
-
+    render () {
+        const todos = this.props.todos
+        const todosList = todos.map(
+            (todo, position) => <li key={ todo+'_'+position }>{ todo }</li>
+        )
+        return (
+            <ul>
+                { todosList }
+            </ul>
+        )
+    }
 }
 export default TodoTasks
