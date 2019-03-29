05609b8b79b6c06af28105fc01cc8aafc99057c5
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-29 09:44:51 +0100

    Wrote changeTodo and addTodo methods to to set currentTodo state and todos state respectively

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,31 @@ class Index extends React.Component
         }
     }
 
-    addTodo (event) {
+    changeTodo = (event) => {
+        this.setState({currentTodo: event.target.value})
+    }
 
+    // Adds this.state.currentTodo if it's not empty to this.state.todos
+    addTodo = (event) => {
+        // To prevent default reload on Submit
+        event.preventDefault()
+        const currentTodo = this.state.currentTodo.trim()
+        if (currentTodo !== "") {
+            this.setState({
+                currentTodo: '',
+                todos: [...this.state.todos, currentTodo] // ES6 Spread Operator
+            })
+        }
     }
 
     render () {
         return (
             <div>
-                <TodoForm />
-                <p>Hello Next.js</p>
+                <TodoForm 
+                    changeTodo={this.changeTodo}
+                    addTodo={this.addTodo}
+                    currentTodo={this.state.currentTodo}
+                />
             </div>
         )
     }
