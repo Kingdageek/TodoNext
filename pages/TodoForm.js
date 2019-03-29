4e560989ff6535646923c530c8625755d1110c07
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-29 09:45:52 +0100

    Set the value of the task input to a prop

--- pages/TodoForm.js
+++ pages/TodoForm.js
@@ -5,7 +5,7 @@ class TodoForm extends React.Component
              <div>
                  <h2>Simple Todo List App</h2>
                  <form onSubmit={this.props.addTodo}>
-                     <input type="text" placeholder="Your task here" onChange={this.props.changeTodo}/>
+                     <input type="text" placeholder="Your task here" onChange={this.props.changeTodo} value={this.props.currentTodo}/>
                      <input type="submit" value="Add Task"/>
                  </form>
              </div>
