42f63a98021c8cabb779bf6c1d65f0b6c9f46c2f
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-04-03 18:56:09 +0100

    Created module containing todos array with dummy todos

---
+++ includes/todos.js
@@ -0,0 +1,7 @@
+module.exports.todos = [
+    "Create express server",
+    "Create todos list",
+    "Learn fetching data in next",
+    "Learn something oddly called getInitialProps"
+]
+// export default todos
